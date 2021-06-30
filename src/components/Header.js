cc7d9c5190a03e23249bcf61dcdda31e12e3a14e
“Jamie <knutsenjamie@yahoo.com>
2021-06-30 09:54:26 -0700

    add tuesday lessons to homework

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from "react";
 import ticketsImage from "./../img/tickets-image.png";
 
 function Header() {
-    return (
-        <React.Fragment>
-            <h1>Help Queue</h1>
-            <img src={ticketsImage} alt="An image of tickets" />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={ticketsImage} alt="tickets" />
+    </React.Fragment>
+  );
 }
 
 export default Header;
